a97ccbaf77dd233745dee166c49ad746648ce608
Denis chuma <chumiks1999@gmail.com>
2023-07-04 02:11:29 +0300

    Add TownPlugin and TownPrePlugin

---
+++ MyPlugin/TownPlugin.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlugin
+{
+    public class TownPlugin : PluginBase
+    {
+        public TownPlugin(string unsecureConfiguration, string secureConfiguration)
+            : base(typeof(PaymentPlugin))
+        {
+        }
+
+        protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
+        {
+            IPluginExecutionContext _context = localPluginContext.PluginExecutionContext;
+
+            Entity town = (Entity)_context.InputParameters["Target"];
+            var regionRef = localPluginContext.Current.GetAttributeValue<EntityReference>("crfe2_region");
+            Entity region = localPluginContext.CurrentUserService.Retrieve(regionRef.LogicalName, regionRef.Id, new ColumnSet("crfe2_people"));
+
+            var people = localPluginContext.Latest.GetAttributeValue<int?>("crfe2_people");
+            var sum = people ?? 0;
+            if (region.Attributes.Contains("crfe2_people") && (int)region.Attributes["crfe2_people"] >= 0)
+            {
+                sum += (int)region.Attributes["crfe2_people"];
+                region.Attributes["crfe2_people"] = sum;
+            }
+            else
+            {
+                region.Attributes.Add("crfe2_people", sum);
+            }
+
+
+            localPluginContext.CurrentUserService.Update(region);
+        }
+    }
+}
---
+++ MyPlugin/TownPrePlugin.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlugin
+{
+    public class TownPrePlugin : PluginBase
+    {
+        public TownPrePlugin(string unsecureConfiguration, string secureConfiguration)
+            : base(typeof(PaymentPlugin))
+        {
+        }
+
+        protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
+        {
+            IPluginExecutionContext _context = localPluginContext.PluginExecutionContext;
+            if (localPluginContext.Target.Contains("crfe2_region"))
+            {
+                Entity town = (Entity)_context.InputParameters["Target"];
+                var people = localPluginContext.Current.GetAttributeValue<int>("crfe2_people");
+                var regionRef = localPluginContext.Current.GetAttributeValue<EntityReference>("crfe2_region");
+                Entity region = localPluginContext.CurrentUserService.Retrieve(regionRef.LogicalName, regionRef.Id, new ColumnSet("crfe2_people", "crfe2_name"));
+                if (region.Attributes.Contains("crfe2_people"))
+                    region.Attributes["crfe2_people"] = (int)region.Attributes["crfe2_people"] - people;
+
+                localPluginContext.CurrentUserService.Update(region);
+            }
+           
+
+
+
+        }
+    }
+}
