8664244e1a2439bee75c92c4e39fcf3c891ce7d0
Denis chuma <chumiks1999@gmail.com>
2023-07-04 11:17:56 +0300

    Fix TownPlugin

--- MyPlugin/TownPlugin.cs
+++ MyPlugin/TownPlugin.cs
@@ -19,7 +19,6 @@ namespace MyPlugin
         protected override void ExecuteCdsPlugin(ILocalPluginContext localPluginContext)
         {
             IPluginExecutionContext _context = localPluginContext.PluginExecutionContext;
-
             Entity town = (Entity)_context.InputParameters["Target"];
             var regionRef = localPluginContext.Current.GetAttributeValue<EntityReference>("crfe2_region");
             Entity region = localPluginContext.CurrentUserService.Retrieve(regionRef.LogicalName, regionRef.Id, new ColumnSet("crfe2_people"));
@@ -35,9 +34,26 @@ namespace MyPlugin
             {
                 region.Attributes.Add("crfe2_people", sum);
             }
-
-
             localPluginContext.CurrentUserService.Update(region);
+            if (localPluginContext.Target.Contains("crfe2_region"))
+            {
+                Entity image = new Entity();
+                if (_context.PreEntityImages.ContainsKey("PreImage"))
+                {
+                    image = (Entity)_context.PreEntityImages["PreImage"];
+                    people = image.GetAttributeValue<int>("crfe2_people");
+                    regionRef = image.GetAttributeValue<EntityReference>("crfe2_region");
+                    region = localPluginContext.CurrentUserService.Retrieve(regionRef.LogicalName, regionRef.Id, new ColumnSet("crfe2_people", "crfe2_name"));
+                    
+                    if (region.Attributes.Contains("crfe2_people"))
+                    {
+                        region.Attributes["crfe2_people"] = (int)region.Attributes["crfe2_people"] - people;
+                    }
+                    localPluginContext.CurrentUserService.Update(region);
+                }
+            }
+
+
         }
     }
 }
